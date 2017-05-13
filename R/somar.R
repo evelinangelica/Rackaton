dc00b4b7cd5ce642aebdecb1d5303e9067385a0d
Evelin Farias <Evelin Farias>
2017-05-13 00:13:15 -0300

    estrutura

---
+++ R/somar.R
@@ -0,0 +1,16 @@
+#' Soma dois Elementos
+#'
+#' somar retorna a soma de dois valores
+#'
+#' @param x primeiro elemento
+#' @param y segundo elemento
+#'
+#' @examples
+#' x <- 1
+#' y <- 2
+#' somar(x,y)
+#'
+#' @export
+somar <- function(x,y) { #### devtools::use_package('tibble')
+  x + y
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![Travis-CI Build Status](https://travis-ci.org/evelinangelica/Rackaton.svg?branch=master)](https://travis-ci.org/evelinangelica/Rackaton)
+
 # Rackaton
 Aprendendo a criar pacotes
